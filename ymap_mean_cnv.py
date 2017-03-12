814efb3b453043e75270f1dd70b19b35afc456f9
fillin12 <procrastrjf@aol.com>
2017-03-12 19:55:48 -0400

    Add files via upload

--- ymap_mean_cnv.py
+++ ymap_mean_cnv.py
@@ -17,13 +17,13 @@ def String_Parser(main_string, str_to_find):
 
     i = 0
 
-    while (i + split_size) <= main_string_length:
+    while ( i + split_size ) <= main_string_length:
 
-        main_string_split = main_string[i:i+split_size]
+        main_string_split = main_string[ i : i+split_size ]
 
         if str_to_find in main_string_split:
 
-            index_list.append(i)
+            index_list.append( i )
 
         i += 1
 
@@ -31,17 +31,25 @@ def String_Parser(main_string, str_to_find):
 
 
 
-def Parse_File( input_file, chr_name, start_pos, end_pos ):
+def Parse_File( input_file, chr_name, start_pos, end_pos, ignore_cnv = 1 ):
 
-    data_list = []
+    '''Looks at a specific location in a given .gff3-formatted text file for the CNV and/or ploidy data'''
+
+    zero_count = 0
+    data_list  = []
 
     for lines in input_file:
 
-        lines_list       = lines.split("\t")
+        lines_list = lines.split("\t")
 
-        string_indicator = String_Parser( lines_list[0], chr_name ) #looks for chromosome name in the line string
+        if len( line_list ) < 9: #Ignore the file header and other odd stuff
+            continue
 
-        if string_indicator != []:
+        if ( line_list[ 5 ] == "0.0" )  and ( ignore_cnv == 1 ): #Ignores sections that have no CNV data
+            zero_count += 1
+            continue
+
+        if String_Parser( lines_list[0], chr_name ) != []:
 
             if ( int( lines_list[4] ) >= int( start_pos ) ) and \
                ( int( lines_list[3] ) <= int(  end_pos  ) ) :
@@ -52,8 +60,11 @@ def Parse_File( input_file, chr_name, start_pos, end_pos ):
 
 
 
-def Grand_File_Parser( input_file ):
+def Grand_File_Parser( input_file, ignore_cnv = 1 ):
 
+    '''Looks through all chromosomes in a given .gff3-formatted text file for the CNV and/or ploidy data'''
+
+    zero_count      = 0
     chr_name_list   = [ "chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chrR" ]
     chr_ploidy_dict = { "chr1": [0, 0], "chr2": [0, 0], "chr3": [0, 0], "chr4": [0, 0], "chr5": [0, 0], "chr6": [0, 0], "chr7": [0, 0], "chrR": [0, 0] }
 
@@ -61,24 +72,31 @@ def Grand_File_Parser( input_file ):
 
         line_list = lines.split() # Splits the line into a list on spaces
 
+        if len( line_list ) < 9: #Ignore the file header and other odd stuff
+            continue
+
+        if ( line_list[ 5 ] == "0.0" )  and ( ignore_cnv == 1 ): #Ignores sections that have no CNV data, adds the number of 
+            zero_count += 1 #can be added to "include" "missing data"
+            continue
+
         i = 0 
 
         while ( i <= 7 ):
 
             if String_Parser( lines, chr_name_list[ i ] ) != []: #if the line contains information pertaining to the chromosome of choice
-
-                chr_ploidy_dict[ chr_name_list[ i ] ][ 0 ] += float( line_list[ 5 ] )
-                chr_ploidy_dict[ chr_name_list[ i ] ][ 1 ] += 1
+                
+                chr_ploidy_dict[ chr_name_list[ i ] ][ 0 ] += float( line_list[ 5 ] ) #adds the current bin's cnv to the total
+                chr_ploidy_dict[ chr_name_list[ i ] ][ 1 ] += 1                       #counts the number of bins
 
             i += 1 
 
-    return chr_ploidy_dict # Format will be [ strain_name, chr_1_ploidy, ... , chrR_ploidy ] 
+    return chr_ploidy_dict # Format will be [ strain_name, chr_1_ploidy, chr_2_ploidy, ... , chrR_ploidy ] 
 
 
 
 def Date_and_Time():
 
-    time_list   = time.asctime( time.localtime(time.time()) ).split()
+    time_list   = time.asctime( time.localtime( time.time() ) ).split()
     time_string = time_list[ 4 ] + "_" + time_list[ 1 ] + "_" + time_list[ 2 ] 
 
     return time_string
@@ -129,10 +147,11 @@ def main():
 
             new_file_name = new_file_name + ".csv"
 
-        file_list       = os.listdir()
-        chr_name_list   = [ "chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chrR" ]
-        output_file     = open( new_file_name, "w" )
+        file_list     = os.listdir()
+        chr_name_list = [ "chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chrR" ]
+        output_file   = open( new_file_name, "w" )
 
+        print( "name, chr1, chr2, chr3, chr4, chr5, chr6, chr7, chrR", file = output_file )
 
         for files in file_list:
 
@@ -156,26 +175,27 @@ def main():
                 except ZeroDivisionError:
 
                     print( "ERROR: Incorrect format in file: ", files )
-                    print( "\tSkipping this file.")
+                    print( "\tSkipping this remaining data in this file." )
                     break
 
-                new_line           = new_line + str( round( average_chr_ploidy, 2 ) ) + ", "
+                new_line = new_line + str( round( average_chr_ploidy, 2 ) ) + ", "
 
                 i += 1
 
-            print( new_line[:-2], file = output_file ) # [:-2] clips off the ", " at the very end of the string
+            print( new_line[:-2], file = output_file ) # [:-2] clips off the ", " (comma delimiter) at the very end of the string
 
 
     elif len( arg_list ) == 5: # Takes average of a highly specified location
 
         try:
 
-            input_file_name  = arg_list[-4]
-            chromosome_name  = arg_list[-3]
-            start_pos        = arg_list[-2]
-            end_pos          = arg_list[-1] 
+            input_file_name  = arg_list[ 1 ] 
+            chromosome_name  = arg_list[ 2 ]
+            start_pos        = arg_list[ 3 ]
+            end_pos          = arg_list[ 4 ] 
 
         except IndexError: 
+
             print( "\nERROR: The command you've entered was not correct. Try the -help command.\n" )
             quit()
 
@@ -191,5 +211,7 @@ def main():
         print( "ERROR: There is something wrong with your command!" )
         Help_Message()
         quit()
+
+    print( "Done!")
 
 main()
